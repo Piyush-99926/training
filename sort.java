64897e13d8908aa15b9b615d707282c910db8c3f
Piyush <chaudharipiyush831@gmail.com>
2025-01-16 14:54:50 +0530

    new

---
+++ sort.java
@@ -0,0 +1,16 @@
+public class sort{
+    public static void main(String[] args){
+        String str = "aabba";
+        int count[] = new int[str.length()];
+        for(int i=0;i<str.length();i++){
+            for(int j=i+1; j<str.length();j++){
+                if(str.charAt(i)==str.charAt(j)){
+                    count[i]++;
+
+                }
+            }
+            System.out.print(count[i]);
+        }
+        
+    }
+}
